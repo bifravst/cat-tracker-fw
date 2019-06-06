3b8992092b92d1ecc58e9c15b164ea794482d083
Simen <simensr@stud.ntnu.no>
2019-06-06 09:23:08 +0200

    cleanup

--- samples/nrf9160/asset_tracker/src/main.c
+++ samples/nrf9160/asset_tracker/src/main.c
@@ -6,24 +6,9 @@
 
 #include <dk_buttons_and_leds.h>
 #include <zephyr.h>
-//#include <gps.h>
 #include <stdio.h>
 #include <uart.h>
 #include <string.h>
-
-// /**@brief Recoverable BSD library error. */
-// void bsd_recoverable_error_handler(uint32_t err)
-// {
-// 	printk("bsdlib recoverable error: %u\n", err);
-// }
-
-// /**@brief Irrecoverable BSD library error. */
-// void bsd_irrecoverable_error_handler(uint32_t err)
-// {
-// 	printk("bsdlib irrecoverable error: %u\n", err);
-
-// 	__ASSERT_NO_MSG(false);
-// }
 
 static void buttons_leds_init(void)
 {
@@ -41,42 +26,6 @@ static void buttons_leds_init(void)
 		}
 	#endif
 }
-
-// static void gps_init(void)
-// {
-// 	int err;
-// 	struct device *gps_dev = device_get_binding(CONFIG_GPS_DEV_NAME);
-// 	struct gps_trigger gps_trig = {
-// 		.type = GPS_TRIG_DATA_READY,
-// 	};
-
-// 	if (gps_dev == NULL) {
-// 		printk("Could not get %s device\n", CONFIG_GPS_DEV_NAME);
-// 		return;
-// 	}
-// 	printk("GPS device found\n");
-
-// 	if (IS_ENABLED(CONFIG_GPS_TRIGGER)) {
-// 		err = gps_trigger_set(gps_dev, &gps_trig,
-// 				gps_trigger_handler);
-
-// 		if (err) {
-// 			printk("Could not set trigger, error code: %d\n", err);
-// 			return;
-// 		}
-// 	}
-
-// 	err = gps_sample_fetch(gps_dev);
-// 	__ASSERT(err == 0, "GPS sample could not be fetched.");
-
-// 	err = gps_channel_get(gps_dev, GPS_CHAN_NMEA, &nmea_data);
-// 	__ASSERT(err == 0, "GPS sample could not be retrieved.");
-// }
-
-// static void gps_data_pull(void)
-// {
-
-// }
 
 void main(void)
 {
